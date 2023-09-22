af4f8c39f4c180071e73f91072e9b441abb0d4dc
Chris Chau <114549705+ChristopherChau@users.noreply.github.com>
2023-09-22 00:57:16 -0700

    added grid component

--- src/PathFindingVisualizer/PathFindingVisualizer.jsx
+++ src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -1,20 +1,45 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { Component } from 'react';
 import Node from './Node/Node';
 
-const PathFindingVisualizer = (props) => {
-  // State should be managed using hooks in functional components
-  const [state, setState] = React.useState({});
+class PathFindingVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nodes: [],
+    };
+  }
 
-  return (
-    <>
-      <div>
-        Foobar
-        {/* <Node></Node> */}
-      </div>
-    </>
-  );
-};
+  componentDidMount() {
+    this.componentCreateGrid();
+  }
+
+  componentCreateGrid() {
+    //create a nodes array where every index represents the row and for every row, we have an array of 50 empty arrays 
+
+    //basically array where we have arrays of empty arrays 
+    const nodes = [];
+    for (let rows = 0; rows < 15; rows++) {
+      let currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push([]);
+      }
+      nodes.push(currentRow);
+    }
+    this.setState({ nodes: nodes });
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          Foobar
+          {/* <Node></Node> */}
+        </div>
+      </>
+    );
+  }
+}
 
 export default PathFindingVisualizer;
