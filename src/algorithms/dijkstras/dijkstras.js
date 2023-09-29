e942040941ad43a0cf035e22831ddc2001e66b90
Chris Chau <114549705+ChristopherChau@users.noreply.github.com>
2023-09-28 20:49:07 -0700

    Started using MinHeap along with map

--- src/algorithms/dijkstras/dijkstras.js
+++ src/algorithms/dijkstras/dijkstras.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
-import helpers from './helper';
+import helpers from './helper'; 
+import {MinHeap} from '../../dataStructure/Heap/minHeap'
 
 
 export function dijkstra(grid, start, finish){
@@ -30,7 +31,35 @@ export function getFinalPath(finishNode){
   return finalPath;
 }
 
+export function dijkstra2(grid, start, finish){
+  let minHeap = new MinHeap();
+  const visitedNodesInOrder2 = [];
+  const distances = new Map();
+  start.distance = 0;
+  minHeap.insert(start, 0);
+  distances[start] = 0;
+  minHeap = getAllNodes2(grid, minHeap, distances); //maybe just need to call function no need for minHeap =
+  while (!minHeap.isEmpty()){
+    const {node, distance} = minHeap.extractMin();
+    if (distance > distances[node]) continue;
+    if (node.isWall) continue;
+    if (node.distance === Infinity) return visitedNodesInOrder2;
+    node.visited = true;
+    if (node === finish) return visitedNodesInOrder2;
+    
+  }
+}
+
 /* go through grid in getAllNodes but instead of pushing into array, we push onto the heap and we return our heap. so now we have all unvisited nodes, we say while heap is not empty, closest node is heap.top and we check if iswall or distance=ifninityy, then we set the currnode to visited and we push it to our visitednodes in order. we then check if the closest node is the finish node and if it isnt, update neighbors passing in our heap, curr node, and grid to update values in the heap then we pop the top */
  
 
 /* push all nodes onto the heap and a map and then when you update the neighbors distance, you also want to push them onto the heap only if the distance we just calculated is less than the currenty distance that it currently has (use a map to key and value the distances) */
+
+function getAllNodes2(grid, minHeap, distances){
+  for (let row of grid){
+    for (let node of row){
+      distances.set(node, node.distance);
+    }
+  }
+  return minHeap; //maybe not needed?
+}
--- src/dataStructure/Heap/minHeap.js
+++ src/dataStructure/Heap/minHeap.js
@@ -58,8 +58,6 @@ export class MinHeap{
   size(){
     return this.heap.length;
   }
-
-
 }
 
 
