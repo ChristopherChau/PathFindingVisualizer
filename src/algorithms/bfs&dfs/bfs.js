49f1d61be101e9d65c8482ecaaf6a78ab0f4717e
Christopher Chau <chrischau04@gmail.com>
2023-10-29 21:37:59 -0700

    First implementation of bfs

--- src/algorithms/bfs&dfs/bfs.js
+++ src/algorithms/bfs&dfs/bfs.js
@@ -4,7 +4,22 @@ import { getAllNeighbors } from "../algHelpers/globalHelpers"; //returns array o
 
 export function bfs(grid, start, finish) {
   const queue = [];
-  
+  let visitedNodesInOrder = [];
+  queue.push(start);
+  while (queue.length !== 0){
+    const node = queue.shift();
+    visitedNodesInOrder.push(node);
+    node.visited = true;
+    if(node === finish) {
+      return visitedNodesInOrder;
+    }
+
+    const neighbors = getAllNeighbors(node, grid);
+    for (let neighbor of neighbors)
+    {
+      queue.push(neighbor);
+    }
+  }
 
 }
 
