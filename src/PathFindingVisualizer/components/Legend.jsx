60dac15cd9f43ef4529776ce6764febc774d6698
Christopher Chau <christopherchau@ucsb.edu>
2024-12-08 11:41:49 -0800

    FEAT(): Legend is done with everything populated and start/finish nodes on grid have icons

--- src/PathFindingVisualizer/components/Legend.jsx
+++ src/PathFindingVisualizer/components/Legend.jsx
@@ -3,29 +3,36 @@ import "../styles/legend.css"; // Import custom styles if needed
 
 const Legend = () => {
     const legendItems = [
-        { label: "Start Node", image: "/path-to-start-image.png" },
-        { label: "End Node", image: "/path-to-end-image.png" },
-        { label: "Weight Node", image: "/images/weight.svg" }, 
-        { label: "Unvisited Node", className: "unvisited-node" },
-        { label: "Visited Node", className: "visited-node" },
-        { label: "Shortest-path Node", className: "shortest-path-node" },
-        { label: "Wall Node", className: "wall-node" },
-      ];
-
-  return (
-    <div className="legend-container">
-      {legendItems.map((item, index) => (
-        <div className="legend-item" key={index}>
-          {item.image ? (
-            <img src={item.image} alt={item.label} className="legend-icon" />
-          ) : (
-            <div className={`legend-box ${item.className}`}></div>
-          )}
-          <span className="legend-label">{item.label}</span>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Legend;
+      { label: "Start Node", image: "https://static.vecteezy.com/system/resources/previews/034/371/682/non_2x/simple-map-pin-icon-location-information-and-the-current-location-icon-vector.jpg" },
+      { label: "End Node", image: "https://static.vecteezy.com/system/resources/thumbnails/023/257/613/small/flag-icon-isolated-illustration-vector.jpg" },
+      { label: "Weight Node", image: "/images/weight.svg" },
+      { label: "Unvisited Node", className: "unvisited-node" },
+      { 
+        label: "Visited Node", 
+        classNames: ["visited-node", "weight-visited-node"]
+      },
+      { label: "Shortest-path Node", className: "shortest-path-node" },
+      { label: "Wall Node", className: "wall-node" },
+    ];
+  
+    return (
+      <div className="legend-container">
+        {legendItems.map((item, index) => (
+          <div className="legend-item" key={index}>
+            {item.image ? (
+              <img src={item.image} alt={item.label} className="legend-icon" />
+            ) : item.classNames ? (
+              item.classNames.map((className, classIndex) => (
+                <div key={classIndex} className={`legend-box ${className}`}></div>
+              ))
+            ) : (
+              <div className={`legend-box ${item.className}`}></div>
+            )}
+            <span className="legend-label">{item.label}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default Legend;
